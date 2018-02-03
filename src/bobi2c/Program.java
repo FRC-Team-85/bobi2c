1e6ac89253437445f5ad18c832153612eae5c17c
Matt Michielsen <matt@michielsen.us>
2018-02-03 11:26:24 -0500

    Removing data register from read command

--- src/bobi2c/Program.java
+++ src/bobi2c/Program.java
@@ -22,7 +22,7 @@ public class Program {
 				try	{
 					device.write(TAKE_READING);
 					Thread.sleep(80);
-					device.read(DATA_REGISTER, buffer, 0, 2);
+					device.read(buffer, 0, 2);
 					short msb = (short)(buffer[0] & 0xFF);
 					short lsb = (short)(buffer[1] & 0xFF);
 					console.println("MSB = " + String.format("0x%02x", buffer[0]) + " (int: " + msb + ")");
