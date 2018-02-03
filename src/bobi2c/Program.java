27b82968fcf421e87d1236666211c443ffd23b63
Matt Michielsen <matt@michielsen.us>
2018-02-03 11:43:44 -0500

    Added separate read and write addresses

--- src/bobi2c/Program.java
+++ src/bobi2c/Program.java
@@ -5,24 +5,23 @@ import com.pi4j.util.Console;
 
 public class Program {
 
-	private static int address = (byte)0x70;
-	
-	
+	private static int writeAddress = (byte)0x70;
 	private static final byte TAKE_READING = (byte)0x51;
-	private static final byte DATA_REGISTER = (byte)0x71;
 	
 	public static void main(String[] args) {
 		final Console console = new Console();
 		byte[] buffer = new byte[2];
+		int readAddress = writeAddress | 0x01;
 		
 		try {
 			I2CBus i2c = I2CFactory.getInstance(I2CBus.BUS_1);
-			I2CDevice device = i2c.getDevice(address);
+			I2CDevice writeDevice = i2c.getDevice(writeAddress);
+			I2CDevice readDevice = i2c.getDevice(readAddress);
 			while(true) {
 				try	{
-					device.write(TAKE_READING);
+					writeDevice.write(TAKE_READING);
 					Thread.sleep(80);
-					device.read(buffer, 0, 2);
+					readDevice.read(buffer, 0, 2);
 					short msb = (short)(buffer[0] & 0xFF);
 					short lsb = (short)(buffer[1] & 0xFF);
 					console.println("MSB = " + String.format("0x%02x", buffer[0]) + " (int: " + msb + ")");
