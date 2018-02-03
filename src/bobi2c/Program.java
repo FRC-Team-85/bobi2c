a0faa34288faa934f1a5ee631682e3b296eb75b0
Matt Michielsen <matt@michielsen.us>
2018-02-03 10:33:28 -0500

    More address/command changes

--- src/bobi2c/Program.java
+++ src/bobi2c/Program.java
@@ -9,8 +9,6 @@ public class Program {
 	
 	
 	private static final byte TAKE_READING = (byte)0x51;
-	private static final int DATA_MSB = 225;
-	private static final int DATA_LSB = 226;
 	
 	public static void main(String[] args) {
 		final Console console = new Console();
@@ -20,10 +18,10 @@ public class Program {
 			I2CDevice device = i2c.getDevice(address);
 			while(true) {
 				try	{
-					device.write(224, TAKE_READING);
-					Thread.sleep(100);
-					int msb = device.read();
-					int lsb = device.read();
+					device.write(TAKE_READING);
+					Thread.sleep(80);
+					int msb = device.read(0);
+					int lsb = device.read(1);
 					console.println("MSB = " + String.format("0x%02x", msb));
 					console.println("LSB = " + String.format("0x%02x", lsb));
 					Thread.sleep(50);
