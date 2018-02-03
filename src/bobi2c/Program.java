2785b1e2ae1f65bcc0ca821cc927fe94b5c77707
Matt Michielsen <matt@michielsen.us>
2018-02-03 11:46:01 -0500

    Trying to specify read and write addresses in the commands

--- src/bobi2c/Program.java
+++ src/bobi2c/Program.java
@@ -15,13 +15,12 @@ public class Program {
 		
 		try {
 			I2CBus i2c = I2CFactory.getInstance(I2CBus.BUS_1);
-			I2CDevice writeDevice = i2c.getDevice(writeAddress);
-			I2CDevice readDevice = i2c.getDevice(readAddress);
+			I2CDevice device = i2c.getDevice(writeAddress);
 			while(true) {
 				try	{
-					writeDevice.write(TAKE_READING);
+					device.write(writeAddress, TAKE_READING);
 					Thread.sleep(80);
-					readDevice.read(buffer, 0, 2);
+					device.read(readAddress, buffer, 0, 2);
 					short msb = (short)(buffer[0] & 0xFF);
 					short lsb = (short)(buffer[1] & 0xFF);
 					console.println("MSB = " + String.format("0x%02x", buffer[0]) + " (int: " + msb + ")");
