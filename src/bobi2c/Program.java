9ae403a48d7dd675808ff1d3924b6e684c17294a
Matt Michielsen <matt@michielsen.us>
2018-02-15 22:57:46 -0500

    Added --set-address command line parameter

--- src/bobi2c/Program.java
+++ src/bobi2c/Program.java
@@ -7,15 +7,37 @@ public class Program {
 
 	private static int writeAddress = (byte)0x70;
 	private static final byte TAKE_READING = (byte)0x51;
+	private static final byte UNLOCK1 = (byte)0xaa;
+	private static final byte UNLOCK2 = (byte)0xa5;
+	
 	
 	public static void main(String[] args) {
 		final Console console = new Console();
 		byte[] buffer = new byte[2];
-		int readAddress = writeAddress | 0x01;
 		
 		try {
 			I2CBus i2c = I2CFactory.getInstance(I2CBus.BUS_1);
 			I2CDevice device = i2c.getDevice(writeAddress);
+			
+			if (args.length > 0 && args[0] == "--set-address")
+			{
+				if (args.length < 2) {
+					console.println("Please enter the address to set the device to.");
+					return;
+				}
+				
+				int newAddress = Integer.parseInt(args[1]);
+				if (newAddress <= 0 || newAddress == 80 || newAddress == 164 || newAddress == 170 || newAddress > 255) {
+					throw new Exception("Address '" + newAddress + "' is invalid.");
+				}
+				
+				device.write(writeAddress, UNLOCK1);
+				device.write(writeAddress, UNLOCK2);
+				device.write(writeAddress, (byte)newAddress);
+				return;
+			}
+			
+			
 			while(true) {
 				try	{
 					device.write(224, TAKE_READING);
